69c1a263014bb0679da300c071dbb0d8c4934875
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-18 15:06:11 +0530

    Add interview slot mapper class

--- src/main/java/edu/icet/mapper/InterviewSlotMapper.java
+++ src/main/java/edu/icet/mapper/InterviewSlotMapper.java
@@ -7,6 +7,36 @@ import edu.icet.model.entity.InterviewSlot;
 
 public class InterviewSlotMapper {
 
+    public static InterviewSlotDTO toDTO(InterviewSlot slot) {
+        if (slot == null) return null;
 
+        return InterviewSlotDTO.builder()
+                .time(slot.getTime())
+                .slotType(slot.getSlotType())
+                .booking(slot.getBooking() != null ?
+                        BookingDTO.builder()
+                                .title(slot.getBooking().getTitle())
+                                .candidate(slot.getBooking().getCandidate())
+                                .interviewer(slot.getBooking().getInterviewer())
+                                .build()
+                        : null)
+                .build();
+    }
+
+    public static InterviewSlot toEntity(InterviewSlotDTO dto) {
+        if (dto == null) return null;
+
+        return InterviewSlot.builder()
+                .time(dto.getTime())
+                .slotType(dto.getSlotType())
+                .booking(dto.getBooking() != null ?
+                        Booking.builder()
+                                .title(dto.getBooking().getTitle())
+                                .candidate(dto.getBooking().getCandidate())
+                                .interviewer(dto.getBooking().getInterviewer())
+                                .build()
+                        : null)
+                .build();
+    }
 
 }

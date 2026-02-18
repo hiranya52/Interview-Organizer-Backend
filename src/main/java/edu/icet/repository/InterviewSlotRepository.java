2f42a083b9381d4d8af7f55ed7b503455044571f
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-18 15:22:04 +0530

    Add JPA Dependency

---
+++ src/main/java/edu/icet/repository/InterviewSlotRepository.java
@@ -0,0 +1,8 @@
+package edu.icet.repository;
+
+import org.hibernate.boot.models.JpaAnnotations;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InterviewSlotRepository extends  {
+}
